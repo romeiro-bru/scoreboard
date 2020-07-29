00e2fb221a007eaaddac3731866030e62742554c
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-07-29 16:18:41 -0300

    Define stopwatch state, Add onClick - stop, start -

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
 class Stopwatch extends React.Component {
+
+    state = {
+        isRunning: false
+    };
+
+    handleStopwatch = () => {
+        this.setState({
+            isRunning: !this.state.isRunning
+        })
+    }
+
     render() {
         return(
             <div className="stopwatch">
                 <h2>Stopwatch</h2>
                 <span className="stopwatch-time">0</span>
-                <button>Start</button>
+                <button onClick={this.handleStopwatch}>
+                    { this.state.isRunning ? 'Stop' : 'Start' }
+                </button>
                 <button>Reset</button>
             </div>
 
