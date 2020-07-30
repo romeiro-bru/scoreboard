426a92619bd41d5c6753d73178c9280dc34fb4d2
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-07-29 22:29:30 -0300

    Optimize Performance with PureComponent

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import Counter from './Counter';
 
-const Player = (props) => {
-    return (
-        <div className="player">
-            <span className="player-name">
-              <button className="remove-player" onClick={ () => props.removePlayer(props.id) }>✖</button>
-               { props.name }
-            </span>
-            <Counter
-                score={props.score}
-                index={props.index}
-                changeScore={props.changeScore}
-            />
-        </div>
-    )
+class Player extends PureComponent {
+    render() {
+        console.log(this.props.name + 'rendered');
+        return (
+            <div className="player">
+                <span className="player-name">
+                  <button className="remove-player" onClick={ () => this.props.removePlayer(this.props.id) }>✖</button>
+                   { this.props.name }
+                </span>
+                <Counter
+                    score={this.props.score}
+                    index={this.props.index}
+                    changeScore={this.props.changeScore}
+                />
+            </div>
+        )
+    }
 }
 
 export default Player;
