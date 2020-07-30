3c18f7322fb42a3be9263a1d2bb1091690a74943
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-07-30 20:54:33 -0300

    Initialize context using CreateContext()

---
+++ src/components/Context/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const ScoreboardContext = React.createContext();
+
+export const Provider = ScoreboardContext.Provider;
+export const Consumer = ScoreboardContext.Consumer;
