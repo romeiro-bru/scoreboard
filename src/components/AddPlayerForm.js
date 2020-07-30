4b710d60584cd95b10a06b3d8ca03bc25a59c871
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-07-29 23:22:03 -0300

    Refs and the DOM - convert from class to function

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -2,24 +2,18 @@ import React from 'react';
 
 class AddPlayerForm extends React.Component {
 
-    state ={
-        value: ''
-    };
-
-    handleValueChange = (event) => {
-        this.setState({ value: event.target.value });
-    }
+    playerInput = React.createRef();
 
     handleSubmit = (event) => {
         event.preventDefault();
-        this.props.AddPlayer(this.state.value);
-        this.setState({ value: '' });
+        this.props.AddPlayer(this.playerInput.current.value);
+        event.currentTarget.reset();
     }
 
     render() {
         return(
             <form onSubmit={this.handleSubmit}>
-                <input type="text" onChange={this.handleValueChange} value={this.state.value} placeholder="Enter a players name"/>
+                <input type="text" ref={this.playerInput} placeholder="Enter a players name"/>
                 <input type="submit" value="Add a player"/>
             </form>
         );
